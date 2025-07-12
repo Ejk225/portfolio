4a22a198899aa3871ab2f45ca8535b5b34cd54be
Eliel <elielkoffi@yahoo.com>
2025-07-12 18:50:15 +0200

    - optimisation de la prise de contact  avec une automatisation qui m'envoie un mails (sur elielkoffi@yahoo.com), en modifiant le fichier javaScript. - amélioration de la partie "compétence technique "(modification du fichier HTML et CSS). - amélioation  de la partie " à propos de moi " - ajoue des différent lien pour les bouttons

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 // Gestion du formulaire de contact
-document.getElementById('contactForm').addEventListener('submit', function(e) {
+document.getElementById("contactForm").addEventListener("submit", async function (e) {
     e.preventDefault();
-    alert('Merci pour votre message ! Je vous réponds bientôt.');
-    this.reset();
+
+    const data = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      reason: document.getElementById("reason").value,
+      message: document.getElementById("message").value,
+      gdpr: document.getElementById("gdpr").checked
+    };
+
+    try {
+      await fetch("https://hook.eu2.make.com/gd9329oow7s4xau6nbf0pmsj3fourub2", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+
+      const feedback = document.createElement('div');
+        feedback.className = 'feedback success';
+        feedback.textContent = '✅ Votre message a été envoyé avec succès !';
+
+        this.appendChild(feedback); // ajoute le message au formulaire
+        this.reset(); // réinitialise le formulaire
+
+        setTimeout(() => {
+        feedback.remove(); // supprime après 5s
+        }, 5000);
+
+    } 
+    
+    catch (error) {
+      const feedback = document.createElement('div');
+        feedback.className = 'feedback error';
+        feedback.textContent = '❌ Une erreur est survenue. Veuillez réessayer.';
+        this.appendChild(feedback);
+
+        setTimeout(() => {
+        feedback.remove();
+        }, 5000);
+
+    }
 });
 
 // Animation sections
